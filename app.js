cea5f8a81a958889e36169f998064dace7b81225
ViggsV <130056822+ViggsV@users.noreply.github.com>
2023-04-16 22:51:35 -0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const asideLeft = document.querySelector('.aside-left');
-
-    hamburger.addEventListener('click', function() {
-        asideLeft.classList.toggle('open');
-    });
-});
